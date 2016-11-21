4faf646e3086dc7d0666c8a262ecc376a660fe93
jay1012 <uniform3000@naver.com>
2016-11-21 19:41:37 +0900

    Modify weather information webview url

--- app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
@@ -17,10 +17,12 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weather);
         WebView01 =  (WebView) findViewById(R.id.WebView_weather);
         WebView01.setWebViewClient(new WebViewClient());
-
+//        String source = "<a href=\"http://www.google.com\"><img src=\"https://pbs.twimg.com/profile_images/762369348300251136/5Obhonwa.jpg\"\n" +
+//                "     alt=\"W3Schools.com\" width=\"150\" height=\"150\"></a>";
+//        WebView01.loadData(source,"text/html","UTF-8");
         WebSettings webSettings = WebView01.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        WebView01.loadUrl("https://weather.com/weather/today/l/37.28,126.84");
+        WebView01.loadUrl("http://m.rp5.ru/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C%EC%9D%98_%EB%82%A0%EC%94%A8");
         Button button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener()
         {
