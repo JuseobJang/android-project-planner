51ff8344b07a066ca3b46e775e9c3b073d0b7f52
jay1012 <uniform3000@naver.com>
2016-11-22 00:56:53 +0900

    add searchview on the mainactivity

--- app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jangjuseob.botduo;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +13,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import static com.example.jangjuseob.botduo.R.id.editText;
+import static com.example.jangjuseob.botduo.R.id.info;
 
 public class MainActivity extends AppCompatActivity {
 
-
+//    EditText edit = (EditText) findViewById(R.id.editText);
+//    String info  = edit.getText().toString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edit = (EditText) findViewById(R.id.editText);
-//        String editText.getText().toString();
+        final EditText edit = (EditText) findViewById(R.id.editText);
+        String info  = edit.getText().toString();
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,35 +35,51 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        final Button button2 = (Button) findViewById(R.id.button5);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY,edit.getText().toString());
+                startActivity(intent);
 
-//        public void onButton2Clicked(View v){
-//
-//
-//
-//            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-//            intent.putExtra(SearchManager.QUERY,editText.getText().toString());
-//            startActivity(intent);
-//        }
+            }
+        });
+
+
 
 
 //
     }
+//    EditText edit = (EditText) findViewById(R.id.editText);
+//    String info = edit.getText().toString();
+//
+//
+//    String info = edit.getText().toString();
+//    TextInfo inf = new TextInfo(info);
 
 
-//    Button button2 = (Button) findViewById(R.id.button5);
-//    button2.setOnClickListener(new View.OnClickListener()
+
+//    public void onButton2Clicked(View v){
 //
-//    {
-//        public void onClick (View v)
-//        {
-//            String data = edit.getText().toString();
-//            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-//            intent.putExtra(SearchManager.QUERY, data);
-//            startActivity(intent);
 //
-//        }
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(inf.getText()));
+////        intent.putExtra(SearchManager.QUERY,edit.getText().toString());
+//        startActivity(intent);
+//    }
+
+//public class TextInfo{
+//    String info;
+//    public TextInfo(String s){
+//        info = s;
 //    }
 //
-//    );
+//    public String getText(){
+//        return info;
+//    }
+//}
+
+
+
 
 }
