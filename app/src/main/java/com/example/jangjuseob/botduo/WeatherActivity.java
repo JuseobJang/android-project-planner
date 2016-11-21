be2894ffa1707344e3e29051b2918efc430beb39
jay1012 <uniform3000@naver.com>
2016-11-21 20:54:07 +0900

    add goback / gofoward button in weather activity

--- app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
@@ -17,8 +17,7 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weather);
         WebView01 =  (WebView) findViewById(R.id.WebView_weather);
         WebView01.setWebViewClient(new WebViewClient());
-//        String source = "<a href=\"http://www.google.com\"><img src=\"https://pbs.twimg.com/profile_images/762369348300251136/5Obhonwa.jpg\"\n" +
-//                "     alt=\"W3Schools.com\" width=\"150\" height=\"150\"></a>";
+//        String source = "<table cellpadding=0 cellspacing=0 width=500 style=\"border:solid 1px #fe2020;font-family:Arial;font-size:12px;background-color:#fedf20\"><tr><td><table width=100% cellpadding=0 cellspacing=0><tr><td width=8 height=30 background=\"http://rp5.ru/informer/htmlinfa/topshl.png\"  bgcolor=#fe2020> </td><td width=* align=center background=\"http://rp5.ru/informer/htmlinfa/topsh.png\" bgcolor=#fe2020><a style=\"color:#ffffff; font-family:Arial;font-size: 12px;\" href=\"http://rp5.ru/292213/ko\"><b>상록 / 안산시</b></a></td><td width=8 height=30 background=\"http://rp5.ru/informer/htmlinfa/topshr.png\" bgcolor=#fe2020> </td></tr></table></td></tr><tr><td valign=top style=\"padding:0;\"><iframe src=\"http://rp5.ru/htmla.php?id=292213&lang=ko&um=00000&bg=%23fedf20&ft=%23ffffff&fc=%23fe2020&c=%23000000&f=Arial&s=12&sc=4\" width=100% height=272 frameborder=0 scrolling=no style=\"margin:0;\"></iframe></td></tr></table>";
 //        WebView01.loadData(source,"text/html","UTF-8");
         WebSettings webSettings = WebView01.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -28,6 +27,24 @@ public class WeatherActivity extends AppCompatActivity {
         {
             public void onClick(View v){
                 finish();
+
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                WebView01.goBack();
+
+
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button4);
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                WebView01.goForward();
+
 
             }
         });
