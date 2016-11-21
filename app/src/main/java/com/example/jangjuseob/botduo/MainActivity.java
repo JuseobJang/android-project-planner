30b699790754c914a5adfe45a129f80a1d2ca9e2
jay1012 <uniform3000@naver.com>
2016-11-21 16:41:54 +0900

    Make 'Weather information' web view and new screen.

--- app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        WebView WebView01 (WebView) findViewById(R.id.WebView_weather);
+//        WebView01.setWebViewClient(new WebViewClient());
+//
+//        WebSettings webSettings = WebView01.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        WebView01.loadUrl("http://m.naver.com");
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener()
@@ -23,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+//
     }
 }
--- app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/WeatherActivity.java
@@ -1,13 +1,33 @@
 package com.example.jangjuseob.botduo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    WebView WebView01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        WebView01 =  (WebView) findViewById(R.id.WebView_weather);
+        WebView01.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = WebView01.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebView01.loadUrl("https://weather.com/weather/today/l/37.28,126.84");
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                finish();
+
+            }
+        });
     }
 }
