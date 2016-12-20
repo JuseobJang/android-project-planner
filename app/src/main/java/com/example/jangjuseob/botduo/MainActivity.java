48410c8e2ce611c07de3c046d76737dd9367e2ee
wntjq68 <wntjq68@hanyang.ac.kr>
2016-12-20 21:01:32 +0900

    delete not needed activity and modify main activity

--- app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
+++ app/src/main/java/com/example/jangjuseob/botduo/MainActivity.java
@@ -33,14 +33,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button3 = (Button) findViewById(R.id.button6);
-        button3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, QueryActivity.class);
-                startActivity(intent);
-            }
-        });
-
         Button button2 = (Button) findViewById(R.id.button5);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
